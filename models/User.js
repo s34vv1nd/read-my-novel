f2cda29264703d95df236adf63d4c5e217eba192
LeMinhHaiPhong <aquariuslee99@gmail.com>
2020-06-08 01:10:00 +0700

    DB name user

--- models/User.js
+++ models/User.js
@@ -20,4 +20,4 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model('user', UserSchema, 'user');
