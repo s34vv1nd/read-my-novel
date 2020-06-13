94bb8c46067a1a41047db55df93512cf90d8e762
minhlh <lhmzz1999@gmail.com>
2020-06-13 21:21:24 +0700

    Library

--- client/src/components/library/Library.jsx
+++ client/src/components/library/Library.jsx
@@ -1,18 +1,72 @@
-import React, { Component } from 'react';
-import {Redirect} from 'react-router-dom';
+import React, { Component, Fragment } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { loadLibrary, removeFromLibrary } from '../../actions/library';
 
 class Library extends Component {
+    constructor() {
+        super();
+        this.state = {
+            bookid: ''
+        }
+
+        this.componentDidMount = this.componentDidMount.bind(this);
+
+    }
+
+    async componentDidMount() {
+        await this.props.loadLibrary();
+    }
+
+    async onClickRemove(e) {
+        await this.setState({
+            bookid: e.target.value
+        });
+
+        await this.props.removeFromLibrary(this.state.bookid);
+    }
+
     render() {
         if (!this.props.isAuthenticated) {
             return <Redirect to='/login' />;
         }
 
         return (
-            <div>
-                <p>Library</p>
-            </div>
+            <>
+                <h2>Library</h2>
+                {(this.props.books && this.props.books[0])?
+                    <Table responsive>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Book name</th>
+                                <th>Book genres</th>
+                                <th>Status</th>
+                                <th>Created at</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.books.map(book =>
+                                <tr key={this.props.books.indexOf(book) + 1}>
+                                    <td>{this.props.books.indexOf(book) + 1}</td>
+                                    <td>{book.name}</td>
+                                    <td>{book.genres.map(genre => `${genre['name'] || genre} `)}</td>
+                                    <td>{book.completed ? "Completed" : "Ongoing"}</td>
+                                    <td>{book.createdAt}</td>
+                                    <td><Button variant="primary" onClick={this.onClickRemove} value={book._id}>Remove from library</Button></td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </Table>
+
+                    :
+                    <p>You haven't added any books yet. It’s time to <Link to='/browse'>EXPLORE</Link></p>
+                }
+
+            </>
         )
     }
 }
@@ -22,9 +76,11 @@ Library.propTypes = {
 };
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+    isAuthenticated: state.auth.isAuthenticated,
+    books: state.browse.books,
 });
 
 export default connect(
     mapStateToProps,
+    { loadLibrary, removeFromLibrary }
 )(Library);
