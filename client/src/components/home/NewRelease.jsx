405fcec880d3e34027f7a04847016f55aa27e8a5
binhlongmm99 <binhlongmm99@gmail.com>
2020-06-16 16:16:08 +0700

    update author

--- client/src/components/home/NewRelease.jsx
+++ client/src/components/home/NewRelease.jsx
@@ -38,6 +38,7 @@ export default class HomeRelease extends Component {
                     <img class="card-img-top" src={this.state.books[0].cover ? this.state.books[0].cover : imgdefault} alt="Book image"></img>
                     <div class="card-body">
                         <h4 class="card-title"><Link to={"book/" + this.state.books[0]._id}>{this.state.books[0].name}</Link></h4>
+                        <p class="card-text">Author: {this.state.books[0].author.username}</p>
                         <p class="card-text">Book intro</p>
                         <a href={"book/" + this.state.books[0]._id} class="btn btn-primary">READ</a>
                     </div>
